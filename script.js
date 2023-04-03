d64fbca31501e35b799cea7b08e04385581202f1
Admin <Admin@MacBook-Pro.local>
2023-04-03 17:32:17 +0300

    buncha stuff

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-function displayProducts (){
+const priceFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'ILS'
+})
+
+const catalogContents = []
+
+const cartContents = []
+
+function displayProducts() {
     fetch('catalog.json')
         .then((response) => response.json())
         .then((data) => {
-            data.forEach(entry => productCard(entry))
+            data.forEach(entry => {
+                productCard(entry)
+                catalogContents.push(entry)
+            })
         })
 }
 
-function productCard (deetz) {
+function productCard(deetz) {
 
     const card = document.createElement('div')
     card.className = 'card'
     card.style.width = '18rem'
+
+    const img = document.createElement('img')
+    img.className = 'card-img-top'
+    img.src = 'res/images/default.jpeg'
+    img.alt = 'book'
 
     const body = document.createElement('div')
     body.className = 'card-body'
@@ -21,19 +38,27 @@ function productCard (deetz) {
 
     const price = document.createElement('p')
     price.className = 'card-text'
-    price.innerHTML = '100' //deetz.price
+    price.innerHTML = priceFormat.format(deetz.pages)
 
     const addButton = document.createElement('button')
     addButton.className = 'btn btn-primary'
     addButton.innerHTML = 'Add to cart'
-    // addButton.onclick = addItemToCart(this)
+    // addButton.onclick = addItemToCart(deetz)
+    addButton.addEventListener('click', () => {
+        addItemToCart(deetz)
+    })
 
     body.appendChild(title)
     body.appendChild(price)
     body.appendChild(addButton)
+    card.appendChild(img)
     card.appendChild(body)
 
     document.getElementById('catalog').appendChild(card)
+}
+
+function addItemToCart (deetz) {
+    cartContents.push(deetz)
 }
 
 displayProducts()
